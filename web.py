da12be0c214f4322e9397b1ba7c1c4847fd8da3d
matc27 <matteo.costanzo1995@gmail.com>
2026-01-02 09:45:37 +0000

    Experiment day 19

--- web.py
+++ web.py
@@ -3,6 +3,8 @@ import functions
 
 todos = functions.get_todos()
 
+st.set_page_config(layout="wide")                                       # set the page configuration like title, icon and layout
+
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"                          # get the value of the input box with sessions_state which is a dictionary
     todos.append(todo)
@@ -10,7 +12,7 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is my todo app")
-st.write("This app is to increase your productivity")
+st.write("This app is to increase your <b>productivity</b>.", unsafe_allow_html=True)
 
 for index, todo in enumerate(todos):                                    # iterate over the todos list to read and print its content
     checkbox = st.checkbox(todo, key=todo)                              # provide dynamic key which is unique for each todo
